4bdedaded87d432f2ca1a7f2bed839c161cde6b1
Danielle Adams <danielle.adams3@extendhealth.com>
2022-06-14 10:56:47 -0600

    ARCH-1213 - Change over to esbuild from ncc.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import * as core from "@actions/core";
-import * as cache from "@martijnhols/actions-cache";
+import * as core from '@actions/core';
+import * as cache from '@martijnhols/actions-cache';
 
 // When used, this requiredArgOptions will cause the action to error if a value has not been provided.
 const requiredArgOptions = {
@@ -8,21 +8,22 @@ const requiredArgOptions = {
 };
 const primaryKey = core.getInput('key', requiredArgOptions);
 const cachePathsRaw = core.getInput('path', requiredArgOptions);
-const cachePaths = cachePathsRaw.split('\n').map(s => s.trim()).filter(x => x !== '');
-
-
+const cachePaths = cachePathsRaw
+  .split('\n')
+  .map(s => s.trim())
+  .filter(x => x !== '');
 
 async function run() {
   try {
-    core.setOutput('cache-hit', primaryKey);
+    core.setOutput('primary-key', primaryKey);
 
     const cacheEntry = await cache.getCacheEntry(cachePaths, primaryKey);
     if (cacheEntry !== null) {
       core.info(`✅ Cache AVAILABLE for input key: ${primaryKey}`);
-      core.setOutput('primary-key', true);
+      core.setOutput('cache-hit', true);
     } else {
       core.info(`❌ Cache MISSING for input key: ${primaryKey}`);
-      core.setOutput('primary-key', true);
+      core.setOutput('cache-hit', true);
     }
   } catch (error) {
     core.setFailed(error.message);
